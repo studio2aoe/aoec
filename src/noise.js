f05908b7b6cf0df5a7b2a389fc8a17f45d389f20
studio2aoe <studio2aoe@gmail.com>
2018-02-20 11:06:12 +0900

    Apply frequency at noise generator

--- src/noise.js
+++ src/noise.js
@@ -1,5 +1,19 @@
 import WaveGenerator from './wavegenerator'
+import AUDIO_CTX from './audioctx'
 import Lfsr from './lfsr'
+
+const SAMPLE_RATE = AUDIO_CTX.sampleRate
+const GCD = function (n, m) {
+  n = Math.abs(n)
+  m = Math.abs(m)
+  if (m > n) { var temp = n; n = m; m = temp }
+  while (true) {
+    if (m === 0) return n
+    n %= m
+    if (n === 0) return m
+    m %= n
+  }
+}
 
 class Noise extends WaveGenerator {
   constructor () {
@@ -7,8 +21,25 @@ class Noise extends WaveGenerator {
     this.lfsr = new Lfsr()
     this.setType(1) // 0: off, 1: long, 2: short
     this.setVol(0xF)
-    this.setFreq(1)
+    this.setFreq(44100)
     this.setInv(0)
+  }
+
+  setFreq (freq) {
+    const gcd = GCD(freq, SAMPLE_RATE)
+    this.freq = freq
+    this.divisedFreq = freq / gcd
+    this.divisedRate = SAMPLE_RATE / gcd
+    this.quotient = Math.floor(this.divisedFreq / this.divisedRate)
+    this.remainder = this.divisedFreq % this.divisedRate
+  }
+
+  clock (phase) {
+    const divisible = (this.remainder * phase) % this.divisedRate < this.remainder
+    const repeat = divisible ? this.quotient + 1 : this.quotient
+    for (let i = 0; i < repeat; i++) {
+      this.lfsr.clock()
+    }
   }
 
   setType (type) {
@@ -19,7 +50,7 @@ class Noise extends WaveGenerator {
 
   calcPhaseValue (phase) {
     let phaseValue = this.lfsr.getHex()
-    if (phase % this.__freq === 0) this.lfsr.clock()
+    this.clock(phase)
     return phaseValue
   }
 }
