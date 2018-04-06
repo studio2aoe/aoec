8c1fc9a941e261c6c79e97f44d11bf658a5f1e78
studio2aoe <studio2aoe@gmail.com>
2018-04-06 17:26:29 +0900

    Add tempo functions on scheduler module
    Add tempo functions
        - Scheduler.setTempo
        - Scheduler.getTempo
        - Scheduler.getPeriod

--- src/scheduler/index.js
+++ src/scheduler/index.js
@@ -1,18 +1,40 @@
-let schedulerFunction = () => {}
+/* Require */
+const check = require('check-types').assert
 
-/**
- * Set scheduler function.
- * @param {Function} func Function executed when processor reads each sample.
- */
-const setFunc = func => { schedulerFunction = func }
+/* Alias */
+const SAMPLE_RATE = 44100
 
-/**
- * Execute scheduler function. it is executed when processor reads each sample.
- * @param {*} clock sampler phase time
- */
-const execute = clock => schedulerFunction(clock)
+/* Structure */
+const SCHEDULER = {
+  tempo: 125,
+  period: 0.02,
+  count: 0,
+  customfunc: (count) => null
+}
+
+/* Private */
+const calcPeriod = (tempo) => {
+  const beatperiod = 60 / tempo
+  const frameperiod = (beatperiod / 24) * SAMPLE_RATE
+  return frameperiod
+}
+/* Public */
+const setTempo = (tempo) => {
+  check.number(tempo)
+  if (tempo < 30) SCHEDULER.tempo = 30
+  if (tempo > 255) SCHEDULER.tempo = 255
+  SCHEDULER.period = calcPeriod(SCHEDULER.tempo)
+  SCHEDULER.count = 0
+}
+const getTempo = () => SCHEDULER.tempo
+const getPeriod = () => SCHEDULER.period
+const setFunc = func => { SCHEDULER.customfunc = func }
+const execute = count => { SCHEDULER.customfunc(count) }
 
 module.exports = {
+  setTempo: setTempo,
+  getTempo: getTempo,
+  getPeriod: getPeriod,
   setFunc: setFunc,
   execute: execute
 }
