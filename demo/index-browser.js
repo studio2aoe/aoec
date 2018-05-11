012799971bed022cd420621c314502e26f3c0471
studio2aoe <studio2aoe@gmail.com>
2018-05-11 13:51:33 +0900

    Add demo.init() for browser demo:
    Because policy of Chrome/Chromium, AudioContext can't be created
    automatically when page is loaded. So, creating AudioContext and
    initializing aoec are included in demo.init()

--- demo/index-browser.js
+++ demo/index-browser.js
@@ -1,26 +1,28 @@
-
 /* Require */
-
 const aoec = require('../src')
 const setupMemory = require('./setup-memory')
 const playInst = require('./play')
 
-/* Setup */
-const AUDIO_CONTEXT = new window.AudioContext()
+let audioContext
+let master
 
-const master = AUDIO_CONTEXT.createGain()
+const init = () => {
+  audioContext = new window.AudioContext()
+  master = audioContext.createGain()
 
-master.connect(AUDIO_CONTEXT.destination)
-master.gain.setValueAtTime(0.5, master.context.currentTime)
+  master.connect(audioContext.destination)
+  master.gain.setValueAtTime(0.5, master.context.currentTime)
 
-aoec.Processor.init(AUDIO_CONTEXT, 4096)
-aoec.Processor.connect(master)
+  aoec.Processor.init(audioContext, 4096)
+  aoec.Processor.connect(master)
 
-setupMemory()
+  setupMemory()
 
-aoec.Instrument.init('OOWN')
-aoec.Scheduler.setTempo(30)
+  aoec.Instrument.init('OOWN')
+  aoec.Scheduler.setTempo(30)
+}
 
 module.exports = {
+  init: init,
   playInst: playInst
 }
